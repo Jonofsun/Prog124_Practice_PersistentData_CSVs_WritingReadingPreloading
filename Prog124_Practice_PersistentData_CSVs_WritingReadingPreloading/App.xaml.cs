d76c635de394b225872acfa7618bcb73e30e79b0
Tenshin <114845631+Jonofsun@users.noreply.github.com>
2023-06-16 21:15:58 -0700

    Initial commit

---
+++ Prog124_Practice_PersistentData_CSVs_WritingReadingPreloading/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Prog124_Practice_PersistentData_CSVs_WritingReadingPreloading
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
